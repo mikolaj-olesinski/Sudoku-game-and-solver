0e44973733cd9d4133edc37e203c6bb023ee6d8f
mikolajolesinski <mikolaj.olesinski11@gmail.com>
2024-06-09 22:27:12 +0200

    nwm

--- pick_sudoku_gui.py
+++ pick_sudoku_gui.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QTableView, QStyledItemDelegate, QWidget, QVBoxLayout,
+from PySide6.QtWidgets import QApplication, QTableView, QStyledItemDelegate, QWidget, QVBoxLayout
 from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex
 from PySide6.QtGui import QMouseEvent
 from model.utils.func import import_data_from_db
@@ -10,18 +10,18 @@ class SudokuModel(QAbstractTableModel):
     def __init__(self, user_id):
         super().__init__()
         self.user_id = user_id
-        self._data = import_data_from_db(user_id)
+        self.sudoku_data = import_data_from_db(user_id)
 
     def rowCount(self, parent=QModelIndex()):
-        return len(self._data)
+        return len(self.sudoku_data)
 
     def columnCount(self, parent=QModelIndex()):
-        return len(self._data[0]) + 4  # Add one for each button column
+        return len(self.sudoku_data[0]) + 4  # Add one for each button column
 
     def data(self, index, role=Qt.DisplayRole):
         if role == Qt.DisplayRole:
             if index.column() < 6:  # Display data for the first five columns
-                return self._data[index.row()][index.column()]
+                return self.sudoku_data[index.row()][index.column()]
 
     def headerData(self, section, orientation, role=Qt.DisplayRole):
         if role == Qt.DisplayRole:
@@ -32,7 +32,7 @@ class SudokuModel(QAbstractTableModel):
     def sort(self, column, order):
         self.layoutAboutToBeChanged.emit()
         if column < 6:  # Sort only for the first five columns
-            self._data.sort(key=lambda x: x[column], reverse=order == Qt.DescendingOrder)
+            self.sudoku_data.sort(key=lambda x: x[column], reverse=order == Qt.DescendingOrder)
         self.layoutChanged.emit()
 
 
@@ -45,15 +45,15 @@ class ButtonDelegate(QStyledItemDelegate):
     def paint(self, painter, option, index):
         if index.column() >= 6:  # Assuming the buttons start from column 6
             button_rect = option.rect
-            data = self.get_path_for_icon(index)
-            if data and os.path.isfile(data):  # Sprawdzanie, czy data jest ścieżką do pliku
-                button_icon = QPixmap(data)
+            sudoku_data = self.get_path_for_icon(index)
+            if sudoku_data and os.path.isfile(sudoku_data):  # Sprawdzanie, czy data jest ścieżką do pliku
+                button_icon = QPixmap(sudoku_data)
                 button_icon = button_icon.scaled(button_rect.size(), Qt.KeepAspectRatio)
                 button_icon_rect = button_icon.rect()
                 button_icon_rect.moveCenter(button_rect.center())
                 painter.drawPixmap(button_icon_rect, button_icon)
-            elif isinstance(data, str):
-                button_text = data
+            elif isinstance(sudoku_data, str):
+                button_text = sudoku_data
                 painter.drawText(button_rect, Qt.AlignCenter, button_text)
             else: 
                 button_text = 'Button'
@@ -65,7 +65,7 @@ class ButtonDelegate(QStyledItemDelegate):
         if index.column() == 6:
             if event.type() == QMouseEvent.MouseButtonRelease:
                 #sciagnij dane z 1 rzedu
-                dane = model._data[index.row()]
+                dane = model.sudoku_data[index.row()]
                 dane = dane[0]
                 self.sudoku_picker.open_sudoku(dane)
 
@@ -93,7 +93,6 @@ class SudokuPicker(QWidget):
         self.layout.addWidget(self.table_view)
         self.cams = None
         self.user_id = user_id
-
 
         self.model = SudokuModel(self.user_id)
         self.table_view.setModel(self.model)
